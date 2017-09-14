e2de222fc69f218b196af0cf9b6f060fd14510b7
emkeator <emkeator@gmail.com>
2017-09-14 15:47:49 -0600

    no more service worker

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import {Provider} from 'react-redux';
-import {HashRouter} from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker';
-// import store from './store';
-// import configureStore from './store';
-// const store = configureStore();
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
     <HashRouter>
         <App />
     </HashRouter>, document.getElementById('root'));
-registerServiceWorker();
+
